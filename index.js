3178914f70f5831741dd41b24b8d495c3527af0c
Samir <hlobhandari909@gmail.com>
2024-01-30 23:14:29 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
-const express = require('express');
-const app = express();
-const {Web3} = require("web3");
-const ABI = require("./ABI.json")
+const express = require('express')
+const cors = require("cors")
+const app = express()
+require('dotenv').config()
+const { Web3 } = require('web3')
+const ABI = require("./ABI")
 
+app.use(cors())
 app.use(express.json())
-const web3 = new Web3("HTTP://127.0.0.1:7545")
-const contractAddress = "0xF9Fd8140a7aE82eFD4dc43fD950A7643c32894E7";
-const contract = new web3.eth.Contract(ABI,contractAddress);
 
-const genderVerification = (gender)=>{
-   const genderData = gender.toLowerCase();
-   if(genderData==="male" || genderData==="female" || genderData==="others"){
-     return true;
-   }else{
-     return false;
-   }
+const API_KEY = process.env.API_KEY
+const PORT = process.env.PORT || 3000;
+
+const web3 = new Web3(`https://eth-sepolia.g.alchemy.com/v2/${API_KEY}`);
+const contractAddress = "0x5D66bf9B0f9CA1546eb6F83caE08f134E7BA6E83";
+//to create contract instance - abi and contract address
+const contract = new web3.eth.Contract(ABI, contractAddress);
+
+
+const genderVerification = (gender) => {
+    const genderValue = gender.toLowerCase()
+    if (genderValue === "male" || genderValue === "female" || genderValue === "others") {
+        return true;
+    }
+    return false;
 }
 
-const partyClashStatus=async(party)=>{
-   const candidateInfo = await contract.methods.candidateList().call();
-   const exists = candidateInfo.some((candidate)=>candidate.party===party);
-   return exists;
+const partyClash = async (party) => {
+    const candidateList = await contract.methods.candidateList().call()
+    const exists = candidateList.some((candidate) => candidate.party.toLowerCase() === party.toLowerCase())
+    return exists;
 }
 
-app.post("/api/voter-verfication",(req,res)=>{
-    const {gender} = req.body;
-    const status = genderVerification(gender)
-    if(status){
-       res.status(200).json({message:"Registration successful"})
-    }else{
-        res.status(403).json({message:"Gender Invalid"})
-    } 
-})
-
-app.post("/api/time-bound",(req,res)=>{
-    const {startTime,endTime}=req.body;
-    if(endTime-startTime<86400){
-        res.status(200).json({message:"Voting Timer Started"})
-    }else{
-        res.status(403).json({message:"Voting Time Must Be Less Than 24 hours"})
+app.post("/api/time-bound", async (req, res) => {
+    const { startTimeSeconds, endTimeSeconds } = req.body;
+    if (endTimeSeconds - startTimeSeconds < 86400) {
+        res.status(200).json({ message: "Voting Time Started" })
+    } else {
+        res.status(403).json({ message: "Time is greater than 24 hours" })
     }
 })
 
-app.post("/api/candidate-verification",async(req,res)=>{
-    const {gender,party}=req.body;
-    const partyStatus = await partyClashStatus(party);
+app.post("/api/voter-verification", async (req, res) => {
+    const { gender } = req.body;
     const genderStatus = genderVerification(gender);
-
-    if(genderStatus===true && partyStatus!==true){
-        res.status(200).json({message:"Registration successfule"})
-    }else{
-        res.status(403).json({message:"Either Party Name or Gender is not valid"})
+    if (genderStatus === true) {
+        res.status(200).json({ message: "Gender Valid" })
+    } else {
+        res.status(403).json({ message: "Gender Value invalid" })
     }
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running")
+app.post("/api/candidate-verification", async (req, res) => {
+    const { gender, party } = req.body;
+    console.log(gender, party)
+    const genderStatus = genderVerification(gender);
+    const partyClashStatus = await partyClash(party);
+    if (genderStatus === true) {
+        if (partyClashStatus === false) {
+            res.status(200).json({ message: "Gender and Party Are Valid" })
+        } else {
+            res.status(403).json({ message: "Either Party Name or Gender is not valid" })
+        }
+    } else {
+        res.status(403).json({ message: "Gender Value invalid" })
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
 })
